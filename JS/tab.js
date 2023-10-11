982e92ef7f7bdcd81904c1aff36a6881cf08c2c6
humi53 <j.k.humi9@gmail.com>
2023-10-11 14:03:12 +0900

    탭 기능 for를 이용하여 구현

---
+++ JS/tab.js
@@ -0,0 +1,25 @@
+/**
+ * 버튼 0 누르면
+ * 모든 버튼?에 붙은 orange 클래스명 제거
+ * 버튼 0에 orange 클래스명 추가
+ * 모든 div에 붙은 show 클래스명 제거
+ * div0에 show 클래스명 추가
+ */
+// $('.tab-button').eq(0).on('click', function() {
+// $('.tab-button').[0].on('click', function() {
+// eq(0)로 해야 jquery 작동 [0]로 하면 바닐라로만 작동.
+$(".tab-button").on("click", function (e) {
+  const tabButtons = $(".tab-button");
+  const tabContents = $(".tab-content");
+  const tabButtonLength = tabButtons.length;
+  const clickedElement = e.target;
+  tabButtons.removeClass("orange");
+  tabContents.removeClass("show");
+  for (i = 0; i < tabButtonLength; i++) {
+    if (tabButtons.eq(i).is(clickedElement)) {
+      tabButtons.eq(i).addClass("orange");
+      tabContents.eq(i).addClass("show");
+      break;
+    }
+  }
+});
