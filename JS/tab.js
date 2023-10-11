578d5c99cddb954db2ea0a2d75c1643a04e64b46
humi53 <j.k.humi9@gmail.com>
2023-10-11 15:07:09 +0900

    이벤트 버블링 응용과 dataset 응용

--- JS/tab.js
+++ JS/tab.js
@@ -8,18 +8,39 @@
 // $('.tab-button').eq(0).on('click', function() {
 // $('.tab-button').[0].on('click', function() {
 // eq(0)로 해야 jquery 작동 [0]로 하면 바닐라로만 작동.
-$(".tab-button").on("click", function (e) {
-  const tabButtons = $(".tab-button");
-  const tabContents = $(".tab-content");
-  const tabButtonLength = tabButtons.length;
-  const clickedElement = e.target;
-  tabButtons.removeClass("orange");
-  tabContents.removeClass("show");
-  for (let i = 0; i < tabButtonLength; i++) {
-    if (tabButtons.eq(i).is(clickedElement)) {
-      tabButtons.eq(i).addClass("orange");
-      tabContents.eq(i).addClass("show");
-      break;
-    }
-  }
+// $(".tab-button").on("click", function (e) {
+//   const tabButtons = $(".tab-button");
+//   const tabContents = $(".tab-content");
+//   const tabButtonLength = tabButtons.length;
+//   const clickedElement = e.target;
+//   tabButtons.removeClass("orange");
+//   tabContents.removeClass("show");
+//   for (let i = 0; i < tabButtonLength; i++) {
+//     if (tabButtons.eq(i).is(clickedElement)) {
+//       tabButtons.eq(i).addClass("orange");
+//       tabContents.eq(i).addClass("show");
+//       break;
+//     }
+//   }
+// });
+
+$(".list").click(function (e) {
+  //지금 누른게 버튼0이면 탭열기(0)
+  탭열기(e.target.dataset.id);
+  // if (e.target == document.querySelectorAll(".tab-button")[0]) {
+  //   탭열기(0);
+  // }
+  // if (e.target == document.querySelectorAll(".tab-button")[1]) {
+  //   탭열기(1);
+  // }
+  // if (e.target == document.querySelectorAll(".tab-button")[2]) {
+  //   탭열기(2);
+  // }
 });
+
+function 탭열기(숫자) {
+  $(".tab-button").removeClass("orange");
+  $(".tab-button").eq(숫자).addClass("orange");
+  $(".tab-content").removeClass("show");
+  $(".tab-content").eq(숫자).addClass("show");
+}
